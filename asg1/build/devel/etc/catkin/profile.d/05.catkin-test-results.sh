9d8dc9c672537d31526413677c909cc5976910ff
Robert <megametal.ninja@gmail.com>
2013-04-15 17:40:38 -0500

    Updated asg1 code for quadcopter; renamed folder and files to respect changes

--- asg1/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.em
-
-export CATKIN_TEST_RESULTS_DIR="/nishome/rlynch/ros/rosbuild_ws/class-code/asg1/build/test_results"
-export ROS_TEST_RESULTS_DIR=$CATKIN_TEST_RESULTS_DIR
--- asg1/build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/nishome/rlynch/ros/rosbuild_ws/class-code/asg1/build/devel/setup.sh"
-emulate zsh # back to zsh mode
