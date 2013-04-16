1f6f0c412217dd80356c519e1544d9b31e3416e7
Josh Eversmann <josh.eversmann@utexas.edu>
2013-04-16 17:40:12 -0500

    Completed 1st listener goal

--- wifi_lookup/src/wifi_listener.py
+++ wifi_lookup/src/wifi_listener.py
@@ -4,25 +4,39 @@ import roslib; roslib.load_manifest('wifi_lookup')
 import rospy, os, re, pickle
 from wifi_lookup.msg import WifiData, Wifi
 
-#1st goal, create a method to build a serialized database which is shared between
+#1st goal, Completed, create a method to build a serialized database which is shared between
 #	different call-time instances of the node
 #2nd goal, redefine the database to perform the two-dimensional hash lookup
 #3rd goal, create member method which publishes location based on lookup and input message
-class ListenNode():
-	#example handler function
-	def output(data):
-		print "I heard ", data.length, " Hotspots"
+dbLoc = "database.pk"
 
-	#create proper handler function to append new hotspots to data object
+def output(data):
+	for spot in data.HotSpots:
+		print spot.MAC, spot.dB
+		database[spot.MAC] = spot.dB
+#create proper handler function to append new hotspots to data object
 
-	def __init__(self):
-	#de-serialize the data object	
-		rospy.Subscriber('wifi_data', WifiData, output)
-		rospy.spin()
-	#create routine to serialize the data object on shutdown
+#serialize the data for storave
+def clean():
+	dbFile = open(dbLoc,"w")
+	pickle.dump(database, dbFile)
+	dbFile.close()
+
+#deserialize the object and do ROS things
+def make():
+	global database
+	try:
+		dbFile = open(dbLoc)
+		database = pickle.load(dbFile)
+		dbFile.close()
+	except: 
+		database = {}
+	rospy.Subscriber('wifi_data', WifiData, output)
+	rospy.spin()
 
 if __name__=='__main__':
 	rospy.init_node('wifi_listener')
 	try:
-		n = ListenNode()
+		make()
 	except rospy.ROSInterruptException: pass
+	clean()
