0a0c6fe6903db2f1ff254d6f194e60211612b070
Josh Eversmann <josh.eversmann@utexas.edu>
2013-04-16 18:03:13 -0500

    Created 2D hash for lookups

--- wifi_lookup/src/wifi_listener.py
+++ wifi_lookup/src/wifi_listener.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 
 import roslib; roslib.load_manifest('wifi_lookup')
-import rospy, os, re, pickle
+import rospy, pickle
 from wifi_lookup.msg import WifiData, Wifi
 
 #1st goal, Completed, create a method to build a serialized database which is shared between
 #	different call-time instances of the node
-#2nd goal, redefine the database to perform the two-dimensional hash lookup
+#2nd goal, Completed, redefine the database to perform the two-dimensional hash lookup
 #3rd goal, modify the listener for x,y injection
+#	make sure that the location injection prevents duplicates, it currently doesn't check
 dbLoc = "database.pk"
 
-def output(data):
+#I'm not sure if all of the layers of temps and resetting are needed, but they are safe
+def inject(data):
 	for spot in data.HotSpots:
 		print spot.MAC, spot.dB
-		database[spot.MAC] = spot.dB
-
-#serialize the data for storave
+		if spot.MAC in database:
+			firstTemp = database[spot.MAC]
+		else:
+			firstTemp = {}
+		if spot.dB in firstTemp:
+			secondTemp = firstTemp[spot.dB]
+		else:
+			secondTemp = []
+		secondTemp.append((0,0)) #currently bad
+		firstTemp[spot.dB] = secondTemp
+		database[spot.MAC] = firstTemp
+	
+#serialize the data for storage
 def clean():
 	dbFile = open(dbLoc,"w")
 	pickle.dump(database, dbFile)
@@ -30,7 +42,7 @@ def make():
 		dbFile.close()
 	except: 
 		database = {}
-	rospy.Subscriber('wifi_data', WifiData, output)
+	rospy.Subscriber('wifi_data', WifiData, inject)
 	rospy.spin()
 
 if __name__=='__main__':
